73b9e7f9aa27976279c293308f68908ff2db095c
sakibesrat71 <esratebtida@iut-dhaka.edu>
2023-11-03 13:01:41 +0600

    tax calc initial commit

---
+++ app.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+
+const bodyParser = require('body-parser');
+const cors = require('cors');
+require('dotenv').config();
+//const fileUpload = require('express-fileupload');
+//middlewares
+app.use(cors());
+app.use(express.json());
+
+// const spell = require('spell-checker-js')
+
+mongoose.set('strictQuery', true);
+
+app.use(function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.header(
+      'Access-Control-Allow-Headers',
+      'Origin, X-Requested-With, Content-Type, Accept'
+    );
+    next();
+});
+
+//authentication module
+const authRoutes = require('./routes/auth');
+app.use('/auth',authRoutes);
+
+
+
+const connectionParams={
+    useNewUrlParser: true,
+    useUnifiedTopology: true 
+}
+
+mongoose.connect(process.env.DB_CONNECTION,
+    connectionParams,
+)
+.then(()=>console.log('DB connected'))
+.catch(e=>console.log(e));
+
+//how do we start listening to the server
+app.listen(5001);
+// create a get route to welcome
+app.get('/',(req,res)=>{
+    res.send('we are on home');
+    res.status(200);
+});
+console.log('listening on port 5001');
+module.exports=app;
